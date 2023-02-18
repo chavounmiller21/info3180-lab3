7bd5e181c7d3978953993b07804f9c80de26ad6d
Chavoun Miller <millerchavoun21@gmail.com>
2023-02-17 20:20:34 -0500

    updated form

--- app/views.py
+++ app/views.py
@@ -1,5 +1,8 @@
 from app import app
 from flask import render_template, request, redirect, url_for, flash
+from .forms import ContactForm
+from app import mail
+from flask_mail import Message
 
 
 ###
@@ -16,6 +19,24 @@ def home():
 def about():
     """Render the website's about page."""
     return render_template('about.html', name="Mary Jane")
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    form = ContactForm()
+
+    if form.validate_on_submit() and request.method == 'POST':
+        msg = Message(
+        subject=form.subject.data,
+        sender=(form.name.data,form.email.data),
+        recipients=['to@example.com']
+    )
+        msg.body = form.message.data
+        mail.send(msg)
+        flash('Form successfully submitted!')
+        return redirect('/')
+    
+
+    return render_template('contact.html',form=form)
 
 
 ###
